e07cb20338e0898a16cc7e17947fccbe8b2c5d5c
unknown <fdsarewqvcxz>
2019-08-28 00:05:57 +0900

    modify config

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -146,7 +145,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 
 # Media files
 MEDIA_URL = '/media/'
