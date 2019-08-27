8ea6cdb7507d6066a1c6da488b2c586593bf9cfa
unknown <fdsarewqvcxz>
2019-08-28 06:47:20 +0900

    aws setting

---
+++ .config/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo rm -rf /etc/nginx/sites-enabled/*
+sudo cp -f /srv/OnlineShop/.config/nginx-app-https.conf /etc/nginx/sites-available/nginx-app.conf
+sudo ln -sf /etc/nginx/sites-available/nginx-app.conf /etc/nginx/sites-enabled/nginx-app.conf
+sudo cp -f /srv/OnlineShop/.config/uwsgi.service /etc/systemd/system/uwsgi.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable uwsgi
+sudo systemctl restart uwsgi nginx
