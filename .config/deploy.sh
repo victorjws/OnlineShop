e65ec096ff55c7518c7a9f2189ac532c9b069f6b
fdsarewqvcxz <fdsarewqvcxz>
2019-09-05 01:59:45 +0900

    letsencrypt

--- .config/deploy.sh
+++ .config/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo rm -rf /etc/nginx/sites-enabled/*
-sudo cp -f /srv/OnlineShop/.config/nginx-app-http.conf /etc/nginx/sites-available/nginx-app.conf
+sudo cp -f /srv/OnlineShop/.config/nginx-app-https.conf /etc/nginx/sites-available/nginx-app.conf
 sudo ln -sf /etc/nginx/sites-available/nginx-app.conf /etc/nginx/sites-enabled/nginx-app.conf
 sudo cp -f /srv/OnlineShop/.config/uwsgi.service /etc/systemd/system/uwsgi.service
 
